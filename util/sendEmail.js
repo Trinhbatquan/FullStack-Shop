9f8b6be6d23e47fec8ba9c7ca8ef1cb9596b059f
Trình <trinhbatquan2001@gmail.com>
2023-09-24 23:39:26 +0700

    fix send email bug

--- util/sendEmail.js
+++ util/sendEmail.js
@@ -2,32 +2,26 @@ const nodemailer = require("nodemailer");
 require("dotenv/config");
 
 module.exports = async (email, subject, html) => {
-  try {
-    console.log(
-      `${process.env.USER_NAME}${process.env.USER_SUB}`,
-      process.env.PASS
-    );
-    const transporter = nodemailer.createTransport({
-      host: process.env.HOST,
-      service: process.env.SERVICE,
-      port: Number(process.env.EMAIL_PORT),
-      secure: Boolean(process.env.SECURE),
-      auth: {
-        user: `${process.env.USER_NAME}${process.env.USER_SUB}`,
-        pass: process.env.PASS,
-      },
-    });
+  console.log(
+    `${process.env.USER_NAME}${process.env.USER_SUB}`,
+    process.env.PASS
+  );
+  const transporter = nodemailer.createTransport({
+    host: process.env.HOST,
+    service: process.env.SERVICE,
+    port: Number(process.env.EMAIL_PORT),
+    secure: Boolean(process.env.SECURE),
+    auth: {
+      user: `${process.env.USER_NAME}${process.env.USER_SUB}`,
+      pass: process.env.PASS,
+    },
+  });
 
-    await transporter.sendMail({
-      from: `From Admin of shop <${process.env.USER_NAME}${process.env.USER_SUB}>`,
-      to: email,
-      subject: subject,
-      html,
-    });
-    console.log("email sent successfully");
-  } catch (error) {
-    console.log("email not sent!");
-    console.log(error);
-    return error;
-  }
+  await transporter.sendMail({
+    from: `From Admin of shop <${process.env.USER_NAME}${process.env.USER_SUB}>`,
+    to: email,
+    subject: subject,
+    html,
+  });
+  console.log("email sent successfully");
 };
