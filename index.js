0e8c6490a4baa78e76c06480919dd1f3ce15b525
Trình <trinhbatquan2001@gmail.com>
2023-10-18 18:30:06 +0700

    change

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const orderRoutes = require("./Routes/OrderRoutes");
 const favoriteRoutes = require("./Routes/FavoriteRoutes");
 const notificationRoutes = require("./Routes/NotificationRoutes");
 
-const request = require("request");
+import request from "request";
 
 // const Notification = require("./Model/Notification");
 
@@ -79,21 +79,25 @@ async function callSendAPI(sender_psid, response) {
 
   // Send the HTTP request to the Messenger Platform
   console.log("send: " + JSON.stringify(request_body));
-  await request(
-    {
-      uri: "https://graph.facebook.com/v2.6/me/messages",
-      qs: { access_token: process.env.PAGE_ACCESS_TOKEN },
-      method: "POST",
-      json: request_body,
-    },
-    (err, res, body) => {
-      if (!err) {
-        console.log("message sent!");
-      } else {
-        console.error("Unable to send message:" + err);
+  try {
+    await request(
+      {
+        uri: "https://graph.facebook.com/v2.6/me/messages",
+        qs: { access_token: process.env.PAGE_ACCESS_TOKEN },
+        method: "POST",
+        json: request_body,
+      },
+      (err, res, body) => {
+        if (!err) {
+          console.log("message sent!");
+        } else {
+          console.error("Unable to send message:" + err);
+        }
       }
-    }
-  );
+    );
+  } catch (e) {
+    console.log(e);
+  }
 }
 // Handles messages events
 async function handleMessage(sender_psid, received_message) {
