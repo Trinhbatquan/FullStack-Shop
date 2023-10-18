c2d8d56fa51ee25936e4e8ceb55a23e26e8ea0e0
Trình <trinhbatquan2001@gmail.com>
2023-10-18 17:22:12 +0700

    check log

--- index.js
+++ index.js
@@ -27,7 +27,7 @@ app.use((req, res, next) => {
 });
 // app.use(express.json());
 app.use(express.json({ limit: "50mb" }));
-app.use(express.urlencoded({ limit: "50mb" }));
+app.use(express.urlencoded({ limit: "50mb", extended: true }));
 
 app.get("/", (req, res) => {
   return res.json("hello world");
