c776a9ae4f253f9ec3344b629a793843d8898fd9
Trình <trinhbatquan2001@gmail.com>
2023-10-19 00:32:09 +0700

    update webhook

--- index.js
+++ index.js
@@ -11,8 +11,7 @@ const favoriteRoutes = require("./Routes/FavoriteRoutes");
 const notificationRoutes = require("./Routes/NotificationRoutes");
 
 const request = require("request");
-
-// const Notification = require("./Model/Notification");
+const { handleMessage, handlePostback } = require("./util/chatbotFacebook");
 
 const app = express();
 app.use(cors({ origin: true }));
@@ -66,104 +65,6 @@ app.get("/api/config/paypal", (req, res) => {
   });
 });
 
-//webhook routes
-// Sends response messages via the Send API
-async function callSendAPI(sender_psid, response) {
-  // Construct the message body
-  let request_body = {
-    recipient: {
-      id: sender_psid,
-    },
-    message: response,
-  };
-
-  // Send the HTTP request to the Messenger Platform
-  console.log("send: " + JSON.stringify(request_body));
-  try {
-    await request(
-      {
-        uri: "https://graph.facebook.com/v2.6/me/messages",
-        qs: { access_token: process.env.PAGE_ACCESS_TOKEN },
-        method: "POST",
-        json: request_body,
-      },
-      (err, res, body) => {
-        if (!err) {
-          console.log("message sent!");
-        } else {
-          console.error("Unable to send message:" + err);
-        }
-      }
-    );
-  } catch (e) {
-    console.log(e);
-  }
-}
-// Handles messages events
-async function handleMessage(sender_psid, received_message) {
-  let response;
-
-  // Checks if the message contains text
-  if (received_message.text) {
-    // Create the payload for a basic text message, which
-    // will be added to the body of our request to the Send API
-    response = {
-      text: `You sent the message: "${received_message.text}". Now send me an attachment!`,
-    };
-  } else if (received_message.attachments) {
-    // Get the URL of the message attachment
-    let attachment_url = received_message.attachments[0].payload.url;
-    response = {
-      attachment: {
-        type: "template",
-        payload: {
-          template_type: "generic",
-          elements: [
-            {
-              title: "Is this the right picture?",
-              subtitle: "Tap a button to answer.",
-              image_url: attachment_url,
-              buttons: [
-                {
-                  type: "postback",
-                  title: "Yes!",
-                  payload: "yes",
-                },
-                {
-                  type: "postback",
-                  title: "No!",
-                  payload: "no",
-                },
-              ],
-            },
-          ],
-        },
-      },
-    };
-  }
-
-  // Send the response message
-  console.log("response: " + JSON.stringify(response));
-  await callSendAPI(sender_psid, response);
-}
-
-// Handles messaging_postbacks events
-function handlePostback(sender_psid, received_postback) {
-  let response;
-
-  // Get the payload for the postback
-  let payload = received_postback.payload;
-
-  // Set the response based on the postback payload
-  if (payload === "yes") {
-    response = { text: "Thanks!" };
-  } else if (payload === "no") {
-    response = { text: "Oops, try sending another image." };
-  }
-  // Send the message to acknowledge the postback
-  callSendAPI(sender_psid, response);
-}
-
 app.post("/webhook", async (req, res) => {
   // Parse the request body from the POST
   let body = req.body;
@@ -183,9 +84,9 @@ app.post("/webhook", async (req, res) => {
       // Check if the event is a message or postback and
       // pass the event to the appropriate handler function
       if (webhook_event.message) {
-        await handleMessage(sender_psid, webhook_event.message);
+        handleMessage(sender_psid, webhook_event.message);
       } else if (webhook_event.postback) {
-        await handlePostback(sender_psid, webhook_event.postback);
+        handlePostback(sender_psid, webhook_event.postback);
       }
     });
 
---
+++ util/chatbotFacebook.js
@@ -0,0 +1,184 @@
+require("dotenv/config");
+const request = require("request");
+
+//webhook routes
+// Sends response messages via the Send API
+// async function gettingStarted() {
+//   // Construct the message body
+//   let request_body = {
+//     get_started: { payload: "GET_STARTED" },
+//     whitelisted_domains: ["https://fullstack-shop-621c2.web.app"],
+//   };
+
+//   // Send the HTTP request to the Messenger Platform
+//   console.log("send: " + JSON.stringify(request_body));
+//   try {
+//     await request(
+//       {
+//         uri: `https://graph.facebook.com/v18.0/me/messenger_profile?access_token=${process.env.PAGE_ACCESS_TOKEN}`,
+//         qs: { access_token: process.env.PAGE_ACCESS_TOKEN },
+//         method: "POST",
+//         json: request_body,
+//       },
+//       (err, res, body) => {
+//         if (!err) {
+//           console.log("set up get started facebook message page!");
+//         } else {
+//           console.error(
+//             "Unable to set up get started facebook message page:" + err
+//           );
+//         }
+//       }
+//     );
+//   } catch (e) {
+//     console.log(e);
+//   }
+// }
+// gettingStarted();
+
+//handleGetStarted
+const getProfileSender = (sender_psid) => {
+  return new Promise(async (resolve, reject) => {
+    try {
+      let userName = "";
+      await request(
+        {
+          uri: `https://graph.facebook.com/${sender_psid}?fields=first_name,last_name,profile_pic&access_token=${process.env.PAGE_ACCESS_TOKEN}`,
+          method: "GET",
+        },
+        async (err, res, body) => {
+          if (!err) {
+            console.log("get profile oke!");
+            const data = await JSON.parse(body);
+            userName = `${data.last_name} ${data.first_name}`;
+          } else {
+            console.error("Unable to send message:" + err);
+          }
+        }
+      );
+      resolve(userName);
+    } catch (e) {
+      reject(e);
+    }
+  });
+};
+const handleGetStarted = async (sender_psid) => {
+  try {
+    const userName = await getProfileSender(sender_psid);
+    const response = `Welcome ${userName} to go the my shop. Have a good day! :))`;
+    await callSendAPI(sender_psid, response);
+  } catch (e) {
+    console.log("error get started: " + e);
+  }
+};
+
+async function callSendAPI(sender_psid, response) {
+  // Construct the message body
+  let request_body = {
+    recipient: {
+      id: sender_psid,
+    },
+    message: response,
+  };
+
+  // Send the HTTP request to the Messenger Platform
+  console.log("send: " + JSON.stringify(request_body));
+  try {
+    await request(
+      {
+        uri: "https://graph.facebook.com/v2.6/me/messages",
+        qs: { access_token: process.env.PAGE_ACCESS_TOKEN },
+        method: "POST",
+        json: request_body,
+      },
+      (err, res, body) => {
+        if (!err) {
+          console.log("message sent!");
+        } else {
+          console.error("Unable to send message:" + err);
+        }
+      }
+    );
+  } catch (e) {
+    console.log(e);
+  }
+}
+
+// Handles messaging_postbacks events
+function handlePostback(sender_psid, received_postback) {
+  let response;
+
+  // Get the payload for the postback
+  let payload = received_postback.payload;
+
+  // Set the response based on the postback payload
+  switch (payload) {
+    case "yes":
+      response = { text: "Thanks!" };
+      break;
+    case "no":
+      response = { text: "Oops, try sending another image." };
+      break;
+    case "GET_STARTED":
+      handleGetStarted(sender_psid);
+      break;
+    default:
+      response = { text: `Opps, I don't know what is the payload: ${payload}` };
+      break;
+  }
+  // Send the message to acknowledge the postback
+  callSendAPI(sender_psid, response);
+}
+
+// Handles messages events
+async function handleMessage(sender_psid, received_message) {
+  let response;
+
+  // Checks if the message contains text
+  if (received_message.text) {
+    // Create the payload for a basic text message, which
+    // will be added to the body of our request to the Send API
+    response = {
+      text: `You sent the message: "${received_message.text}". Now send me an attachment!`,
+    };
+  } else if (received_message.attachments) {
+    // Get the URL of the message attachment
+    let attachment_url = received_message.attachments[0].payload.url;
+    response = {
+      attachment: {
+        type: "template",
+        payload: {
+          template_type: "generic",
+          elements: [
+            {
+              title: "Is this the right picture?",
+              subtitle: "Tap a button to answer.",
+              image_url: attachment_url,
+              buttons: [
+                {
+                  type: "postback",
+                  title: "Yes!",
+                  payload: "yes",
+                },
+                {
+                  type: "postback",
+                  title: "No!",
+                  payload: "no",
+                },
+              ],
+            },
+          ],
+        },
+      },
+    };
+  }
+
+  // Send the response message
+  console.log("response: " + JSON.stringify(response));
+  await callSendAPI(sender_psid, response);
+}
+
+module.exports = {
+  handleMessage,
+  handlePostback,
+};
