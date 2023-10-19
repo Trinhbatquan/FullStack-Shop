690436eb0cd475f6a341f46cff8749cbe79a51df
Trình <trinhbatquan2001@gmail.com>
2023-10-19 10:41:56 +0700

    webhook

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ const favoriteRoutes = require("./Routes/FavoriteRoutes");
 const notificationRoutes = require("./Routes/NotificationRoutes");
 
 const request = require("request");
-const { handleMessage, handlePostback } = require("./util/chatbotFacebook");
+// const { handleMessage, handlePostback } = require("./util/chatbotFacebook");
 
 const app = express();
 app.use(cors({ origin: true }));
@@ -120,8 +120,81 @@ app.get("/webhook", (req, res) => {
   }
 });
 
-// handlePostback("7605071802841718", { payload: "GET_STARTED" });
-// handleMessage("7605071802841718", { text: "hello" });
+function handleMessage(senderPsid, receivedMessage) {
+  let response;
+
+  // Checks if the message contains text
+  if (receivedMessage.text) {
+    // Create the payload for a basic text message, which
+    // will be added to the body of your request to the Send API
+    response = {
+      text: `You sent the message: '${receivedMessage.text}'. Now send me an attachment!`,
+    };
+  } else if (receivedMessage.attachments) {
+    // Get the URL of the message attachment
+    let attachmentUrl = receivedMessage.attachments[0].payload.url;
+    response = {
+      attachment: {
+        type: "template",
+        payload: {
+          template_type: "generic",
+          elements: [
+            {
+              title: "Is this the right picture?",
+              subtitle: "Tap a button to answer.",
+              image_url: attachmentUrl,
+              buttons: [
+                {
+                  type: "postback",
+                  title: "Yes!",
+                  payload: "yes",
+                },
+                {
+                  type: "postback",
+                  title: "No!",
+                  payload: "no",
+                },
+              ],
+            },
+          ],
+        },
+      },
+    };
+  }
+
+  // Send the response message
+  callSendAPI(senderPsid, response);
+}
+
+function callSendAPI(senderPsid, response) {
+  // The page access token we have generated in your app settings
+  const PAGE_ACCESS_TOKEN = process.env.PAGE_ACCESS_TOKEN;
+
+  // Construct the message body
+  let requestBody = {
+    recipient: {
+      id: senderPsid,
+    },
+    message: response,
+  };
+
+  // Send the HTTP request to the Messenger Platform
+  request(
+    {
+      uri: "https://graph.facebook.com/v2.6/me/messages",
+      qs: { access_token: PAGE_ACCESS_TOKEN },
+      method: "POST",
+      json: requestBody,
+    },
+    (err, _res, _body) => {
+      if (!err) {
+        console.log("Message sent!");
+      } else {
+        console.error("Unable to send message:" + err);
+      }
+    }
+  );
+}
 
 app.listen(4000, () => {
   console.log("server running....");
